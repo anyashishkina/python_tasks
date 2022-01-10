8b7e9d21135e0816dda2e21536109401dda77f8d
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-01-10 21:56:34 +0300

    Create 4256.py

---
+++ ege/8/4256.py
@@ -0,0 +1,20 @@
+from itertools import permutations 
+cmd = list(permutations('КУСАТЬ', 5))
+cmd2 = [list(c) for c in cmd]
+k = 0
+def is_right(comb):
+    if comb[0] == 'Ь':
+        return False
+    if 'СУК' in comb:
+        return False
+    for i in range(len(comb)-1):
+        if comb[i] in cmd:
+            k += 1 
+            if k > 1:
+                return False
+    return True 
+result = []
+for c in cmd:
+    if is_right(c):
+        result.append(c)
+print(len(result))
