39ee28f6b80f3744e4919d44c638197cfd81186f
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-01-15 18:27:24 +0300

    Create 4249.py

---
+++ ege/8/4249.py
@@ -0,0 +1,15 @@
+from itertools import permutations
+cmd = list(permutations('СОТКАПЛЗ', 5))
+cmd2 = [list(c) for c in cmd]
+def is_right(comb):
+    if comb[4] in ['О, А']:
+        return False
+    if 'ЗЛО'in comb:
+        return False
+    return True 
+result = []
+for c in cmd:
+    if is_right(c):
+        result.append(c)
+print(len(result))
+#6720
