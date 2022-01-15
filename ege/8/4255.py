993f02379df312ccbde4c715c6626056801b964a
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-01-15 17:41:14 +0300

    Update 4255.py

--- ege/8/4255.py
+++ ege/8/4255.py
@@ -3,7 +3,7 @@ cmd = list(permutations('01234567', 4))
 cmd2 = [list(c) for c in cmd]
 def is_right(comb):
     for i in range(len(comb)):
-        if int(comb[i]) % 4 != 0:
+        if int(''.join(comb[i])) % 4 != 0:
             return False
     return True
 result = []
