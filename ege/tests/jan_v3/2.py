5c9e02b5fa017f1645d1d700fdb57f9a3e0f8943
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-01-19 21:46:25 +0300

    Create 2.py

---
+++ ege/tests/jan_v3/2.py
@@ -0,0 +1,8 @@
+print('x y z w')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                f = (x == (w or y)) or ((w <= z) and (y <= w))
+                if f == False:
+                    print(x,y,z,w)
