08b00ddecb2b7239e984ace13902a36641fffab9
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-01-22 17:15:51 +0300

    Update 4254.py
    теперь выводит 2072, а в ответе 1744

--- ege/8/4254.py
+++ ege/8/4254.py
@@ -1,10 +1,10 @@
-from itertools import permutations
-cmd = list(permutations('01234', 5))
+from itertools import product
+cmd = list(product('01234', repeat = 5))
 cmd2 = [list(c) for c in cmd]
 def is_right(comb):
     k = 0 
     for i in range(len(comb)):
-        if comb[i] in [0,2,4]:
+        if int(comb[i]) in [0,2,4]:
             k += 1 
         if k > 3:
             return False
@@ -14,4 +14,3 @@ for c in cmd:
     if is_right(c):
         result.append(c)
 print(len(result))
-#выводит 120
