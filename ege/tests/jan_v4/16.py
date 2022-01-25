b8067489f6576b01092edb75ef97d15d32970f4e
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-01-25 22:39:00 +0300

    Create 16.py

---
+++ ege/tests/jan_v4/16.py
@@ -0,0 +1,13 @@
+def F(n):
+    if n == 1:
+        return 1 
+    if n > 1:
+        return 2 * G(n - 1) + 5 * n
+        
+def G(n):
+    if n == 1:
+        return 1 
+    if n > 1:
+        return F(n - 1) + 2 * n
+        
+print(F(4) + G(4))
