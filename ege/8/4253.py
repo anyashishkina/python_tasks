4e8a327f0461c5bb483d4e24fbb5914822f0510f
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-01-29 17:38:32 +0300

    Update 4253.py

--- ege/8/4253.py
+++ ege/8/4253.py
@@ -1,8 +1,8 @@
-from itertools import permutations
-cmd = list(permutations('01234567', 4))
+from itertools import product
+cmd = list(product('01234567', repeat = 4))
 cmd2 = [list(c) for c in cmd]
 def is_right(comb):
-    if comb[0] in [0, 2, 4, 6]:
+    if comb[0] in [1, 3, 5, 7]:
         return False
     for i in range(len(comb)-1):
         if comb[i] < comb[i+1]:
@@ -13,4 +13,4 @@ for c in cmd:
     if is_right(c):
         result.append(c)
 print(len(result))
-#выводит 70
+#выводит 330
