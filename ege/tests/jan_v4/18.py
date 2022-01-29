e63caa5889a8c00a81fe1cd4c5d4f4a44fa24edc
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-01-29 14:46:17 +0300

    Create 18.py

---
+++ ege/tests/jan_v4/18.py
@@ -0,0 +1,13 @@
+for x in range(1, 10000):
+    t = x
+    a=0; b=0
+    while x > 0:
+        if x%2 > 0:
+            a += 1
+        else:
+            b += x%6
+        x = x//6
+    if a == 2 and b == 6:
+        print(t)
+        break
+ 
