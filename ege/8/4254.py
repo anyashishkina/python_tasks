b964d16277fe74919eec730e4416faf2e9176f57
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-01-29 21:33:09 +0300

    Create 4254.py

--- ege/8/4254.py
+++ ege/8/4254.py
@@ -3,6 +3,8 @@ cmd = list(product('01234', repeat = 5))
 cmd2 = [list(c) for c in cmd]
 def is_right(comb):
     k = 0 
+    if comb[0] == '0':
+        return False
     for i in range(len(comb)):
         if int(comb[i]) in [0,2,4]:
             k += 1 
