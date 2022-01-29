fdff9c3cb567703093b2893df456d1050ff755cf
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-01-29 21:19:00 +0300

    Update 4447.py

--- ege/8/4447.py
+++ ege/8/4447.py
@@ -1,5 +1,5 @@
 from itertools import permutations 
-cmd = list(permutations('МАРИНА', 6))
+cmd = list(set(permutations('МАРИНА', 6)))
 cmd2 = [list(c) for c in cmd]
 def is_right(comb):
     if comb[0] in 'АИ':
