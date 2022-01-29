443183866eca65c782d94e8f536409b617d90065
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-01-29 17:56:35 +0300

    Update 4256.py
    не понимаю , как здесь применить join в 7 строке

--- ege/8/4256.py
+++ ege/8/4256.py
@@ -2,15 +2,12 @@ from itertools import permutations
 cmd = list(permutations('КУСАТЬ', 5))
 cmd2 = [list(c) for c in cmd]
 def is_right(comb):
-    k = o
     if ''.join(comb[0]) == 'Ь':
         return False
     if 'СУК' in comb:
         return False
     for i in range(len(comb)-1):
-        if comb[i] in cmd:
-            k += 1 
-            if k > 1:
+        if comb.count(comb[i]) > 1:
                 return False
     return True 
 result = []
