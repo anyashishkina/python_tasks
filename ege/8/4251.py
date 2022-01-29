1c77d1b4faf7aed2f74a836769bcd08b836e74bc
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-01-29 17:29:03 +0300

    Update 4251.py

--- ege/8/4251.py
+++ ege/8/4251.py
@@ -1,18 +1,16 @@
-from itertools import permutations
-cmd = list(permutations('ЗЕРКАЛО', 6))
+from itertools import product
+cmd = list(product('ЗЕРКАЛО', repeat = 6))
 cmd2 = [list(c) for c in cmd]
 def is_right(comb):
-    k1 = 0 
+    k = 0 
     k2 = 0
     for i in range(len(comb)-1):
         if comb[i] == 'К':
-            k1 += 1 
-        if k1 > 4:
-            return False
-        if comb[i] in comb:
-            k2 += 1 
-        if k2 > 1:
-            return False
+            k = comb.count('К') 
+            if k > 4:
+                return False
+        if comb.count(comb[i]) > 1:
+           return False
     return True 
 result = []
 for c in cmd:
