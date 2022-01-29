aebbd310b0e2da5270cbf8fd81c691934aa3ce9a
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-01-29 21:09:47 +0300

    Update 4252.py

--- ege/8/4252.py
+++ ege/8/4252.py
@@ -1,5 +1,5 @@
 from itertools import permutations
-cmd = list(permutations('ОДЕКОЛОН', 8))
+cmd = list(set(permutations('ОДЕКОЛОН', 8)))
 cmd2 = [list(c) for c in cmd]
 def is_right(comb):
     for i in range(len(comb)-1):
@@ -11,4 +11,3 @@ for c in cmd:
     if is_right(c):
         result.append(c)
 print(len(result))
-#выводит 1200, в два раза меньще ответа
