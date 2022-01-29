60f03343934dc23fcb47d01932e1369c1e34f52a
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-01-29 21:13:53 +0300

    Update 4253.py

--- ege/8/4253.py
+++ ege/8/4253.py
@@ -2,7 +2,7 @@ from itertools import product
 cmd = list(product('01234567', repeat = 4))
 cmd2 = [list(c) for c in cmd]
 def is_right(comb):
-    if comb[0] in [1, 3, 5, 7]:
+    if int(comb[0]) in [0, 1, 3, 5, 7]:
         return False
     for i in range(len(comb)-1):
         if comb[i] < comb[i+1]:
@@ -13,4 +13,3 @@ for c in cmd:
     if is_right(c):
         result.append(c)
 print(len(result))
-#выводит 330
