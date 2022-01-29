490e62e3d497d276774235ebc4041b3d822f0df1
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-01-29 17:44:51 +0300

    Update 4255.py
    теперь выводит 16

--- ege/8/4255.py
+++ ege/8/4255.py
@@ -1,9 +1,9 @@
-from itertools import permutations
-cmd = list(permutations('01234567', 4))
+from itertools import product
+cmd = list(product('01234567', repeat = 4))
 cmd2 = [list(c) for c in cmd]
 def is_right(comb):
     for i in range(len(comb)):
-        if int(''.join(comb[i])) % 4 != 0:
+        if int(''.join(comb[i]), base = 8) % 4 != 0:
             return False
     return True
 result = []
