03a16b849b7320761ae7acd1f2f10f3c577de997
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-01-29 21:29:54 +0300

    Update 4251.py

--- ege/8/4251.py
+++ ege/8/4251.py
@@ -2,13 +2,9 @@ from itertools import product
 cmd = list(product('ЗЕРКАЛО', repeat = 6))
 cmd2 = [list(c) for c in cmd]
 def is_right(comb):
-    k = 0 
-    k2 = 0
-    for i in range(len(comb)-1):
-        if comb[i] == 'К':
-            k = comb.count('К') 
-            if k > 4:
-                return False
+    if comb.count('К') > 4:
+        return False
+    for i in range(len(comb)):
         if comb.count(comb[i]) > 1:
            return False
     return True 
