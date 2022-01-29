d83a583121c6d4260cfb4bff1196fbb1fbd98070
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-01-29 21:17:46 +0300

    Update 4255.py

--- ege/8/4255.py
+++ ege/8/4255.py
@@ -2,8 +2,10 @@ from itertools import product
 cmd = list(product('01234567', repeat = 4))
 cmd2 = [list(c) for c in cmd]
 def is_right(comb):
+    if comb[0] == '0':
+        return False
     for i in range(len(comb)):
-        if int(''.join(comb[i]), base = 8) % 4 != 0:
+        if int(''.join(comb), base = 8) % 4 != 0:
             return False
     return True
 result = []
