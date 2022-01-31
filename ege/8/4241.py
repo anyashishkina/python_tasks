c86417aee3d282d18ca56b8d2e1b6a3874fb20d6
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-02-01 01:03:20 +0300

    Create 4241.py

---
+++ ege/8/4241.py
@@ -0,0 +1,13 @@
+from itertools import permutations
+cmd = list(set(permutations('КЛАБХАУС', 8)))
+cmd2 = [list(c) for c in cmd]
+def is_right(comb):
+    for i in range(len(comb) - 1):
+        if comb[i] == comb [i+1]:
+            return False
+    return True
+result = []
+for c in cmd:
+    if is_right(c):
+        result.append(c)
+print(len(result))
