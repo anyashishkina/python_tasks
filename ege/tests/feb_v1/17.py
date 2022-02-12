0166fd3f2383d3f051d303fb9b97eb916d164570
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-02-12 20:01:48 +0300

    Update 17.py

--- ege/tests/feb_v1/17.py
+++ ege/tests/feb_v1/17.py
@@ -5,11 +5,12 @@ while True:
     if s == '':
         break
     d.append(int(s))
-for i in range(len(d)-1):
+for i in range(len(d)):
     if d[i] % 2 == 0:
         sum_ch += d[i]
         cnt_ch += 1 
     ave = sum_ch / cnt_ch
+for i in range(len(d)-1):
     if ((d[i] % 3 == 0) or (d[i+1] % 3 == 0)) and ((d[i] < ave) or (d[i+1] < ave)):
         cnt += 1 
         max_s = max(max_s, d[i] + d[i+1])
