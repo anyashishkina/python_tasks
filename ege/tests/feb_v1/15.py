1a1d4bd8faa65671fa1bdb8c8861ae00da3f4471
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-02-12 19:59:36 +0300

    Create 15.py

---
+++ ege/tests/feb_v1/15.py
@@ -0,0 +1,11 @@
+for a in range(1, 100):
+    f = 1 
+    for x in range(1, 100):
+        for y in range(1, 100):
+            if not((2*x + 3*y < 30) or (x + y >= a)):
+                f = 0 
+                break
+        if f == 0:
+            break
+    if f:
+        print(a)
