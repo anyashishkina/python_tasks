570ccbe3990c5ae51c88eba7ca062a50002c6fa1
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-02-18 22:26:34 +0300

    Create 3909.py
    выводит 737, а в ответе 480

---
+++ ege/5/3909.py
@@ -0,0 +1,20 @@
+for n in range(2, 750):
+    r = n 
+    r = bin(r)[2:]
+    if r.count('1') == r.count('0'):
+        r = str(r) + str(r[-1])
+    else:
+        if r.count('1') > r.count('0'):
+            r = str(r) + '0' 
+        else:
+            r = str(r) + '1'
+    if r.count('1') == r.count('0'):
+        r = str(r) + str(r[-1])
+    else:
+        if r.count('1') > r.count('0'):
+            r = str(r) + '0' 
+        else:
+            r = str(r) + '1'
+    r = int(r, base = 2)
+    if r % 2 == 0 and r % 4 != 0:
+        print(n)
