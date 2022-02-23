69aa0c720d28dfd28ec5a753bc2b1845ecd12819
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-02-23 15:35:24 +0300

    Update 17.py

--- ege/tests/feb_v2/17.py
+++ ege/tests/feb_v2/17.py
@@ -1,4 +1,4 @@
-f = open('17 (6).txt')
+f = open('17 (7).txt')
 d, sum_nech, cnt_nech, cnt, max_s = [], 0, 0, 0, 0
 while True:
     s = f.readline()
@@ -9,9 +9,8 @@ for i in range(len(d)):
     if d[i] % 2 != 0:
         sum_nech += d[i]
         cnt_nech += 1 
-    ave = sum_ch / cnt_ch
 for i in range(len(d)-1):
-    if ((d[i] % 5 == 0) or (d[i+1] % 5 == 0)) and ((d[i] < ave) or (d[i+1] < ave)):
+    if ((d[i] % 5 == 0) or (d[i+1] % 5 == 0)) and ((d[i] < sum_nech / cnt_nech) or (d[i+1] < sum_nech / cnt_nech)):
         cnt += 1 
         max_s = max(max_s, d[i] + d[i+1])
 print(cnt, max_s)
