2458270c19c1bd0c7f83594ddc86999dcbdc456e
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-03-03 21:39:02 +0300

    Update and rename 27991.py to 27991A.py
    у меня в ответе 8759, 7083; а правильный- 8759, 3077

--- ege/27/27991A.py
+++ ege/27/27991A.py
@@ -1,4 +1,4 @@
-f = open('27991_B.txt')
+f = open('27991_A.txt')
 m17, m_ch1, m_ch2, m_nech1, m_nech2, s1, s2 = 0, 0, 0, 0, 0, 0, 0
 d = []
 while True:
@@ -7,13 +7,13 @@ while True:
         break
     d.append(int(s))
 for i in range(len(d)):
-    if (d[i] % 2 == 0) and (d[i] > m_ch1):
+    if (d[i] % 2 == 0) and (d[i] > m_ch1) and (d[i] != m_ch2):
         m_ch1 = d[i]
-    if (d[i] % 2 == 0) and (d[i] > m_ch2):
+    if (d[i] % 2 == 0) and (d[i] > m_ch2) and (d[i] != m_ch1):
         m_ch2 = d[i]
-    if (d[i] % 2 != 0) and (d[i] > m_nech1):
+    if (d[i] % 2 != 0) and (d[i] > m_nech1) and (d[i] != m_nech2):
         m_nech1 = d[i]
-    if (d[i] % 2 != 0) and (d[i] > m_nech2):
+    if (d[i] % 2 != 0) and (d[i] > m_nech2) and (d[i] != m_nech1):
         m_nech2 = d[i]
     if (m_ch1 % 17 == 0) or (m_ch2 % 17 == 0):
         s1 = m_ch1 + m_ch2
