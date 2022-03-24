b9e119e00aec28d18ab4ce476967d87d0248ccc7
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-03-24 22:06:25 +0300

    Update 17.py

--- ege/tests/march_v1/17.py
+++ ege/tests/march_v1/17.py
@@ -1,13 +1,16 @@
-f = open('17 (8).txt')
+f = open('17 (10).txt')
 d = []
 cnt, max_s = 0, 0
 while True:
     s = f.readline()
     if s == '':
-        break 
+        break
     d.append(int(s))
 for i in range(len(d)-1):
-    if ((d[i] % 160) != (d[i+1] % 160)) and ((d[i] % 7 == 0) or (d[i+1] % 7 == 0)):
-        cnt += 1 
-        max_s = max(max_s, d[i] + d[i+1])
+    for j in range(i+1, len(d)):
+        if ((d[i] % 160) != (d[j] % 160)) and ((d[i] % 7 == 0) or (d[j] % 7 == 0)):
+            cnt += 1
+            max_s = max(max_s, d[i] + d[j])
+
 print(cnt, max_s)
+
