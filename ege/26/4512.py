acd86201a741764b5d49972af82a7d3aea9effa8
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-04-15 19:53:04 +0300

    Create 4512.py

---
+++ ege/26/4512.py
@@ -0,0 +1,23 @@
+file_content = open('26-61.txt')
+file_lines = file_content.split('\n')
+image_number, image_volume = file_lines[0].split()
+
+images = [int(x) for x in file_lines[1:] if x]
+images.sort()
+s, cnt, max_volume = 0, 0, 0
+
+for image in images:
+    if image >= 101:
+        while s < (image_volume//2):
+            s += image
+            cnt += 1
+    images = images[index(image):]
+
+for image in images:
+    if image < 101:
+        while s < image_volume:
+            s += image
+            cnt += 1
+            max_volume = max(max_volume, image)
+
+print(cnt, max_volume)
