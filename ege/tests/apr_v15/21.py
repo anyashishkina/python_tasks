723d57eae6e7435fc993dffc592dde2f141299ae
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-04-17 15:33:30 +0300

    Create 21.py

---
+++ ege/tests/apr_v15/21.py
@@ -0,0 +1,15 @@
+def game(l, r, h):
+    if ((h == 4) or (h == 2)) and (l + r >= 88):
+        return True
+    if (h == 4) and (l + r < 88):
+        return False
+    if (h < 4) and (l + r >= 88):
+        return False
+    h += 1
+    if h == 1 or h == 3:
+        return game(l+1, r, h) and game(l*3, r, h) and game(l, r+1, h) and game(l, r*3, h)
+    if h == 2 or h == 4:
+        return game(l+1, r, h) or game(l*3, r, h) or game(l, r+1, h) or game(l, r*3, h)
+for x in range(1, 72):
+    if game(6, x, 0):
+        print(x)
