b45d29e9983598da5556534ab3ca2f206c9df295
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-04-18 19:44:39 +0300

    Update 38604reshy_A_B.py

--- ege/27/38604reshy_A_B.py
+++ ege/27/38604reshy_A_B.py
@@ -1,6 +1,6 @@
 f = open('27_B (1).txt')
 n = int(f.readline())
-sum_ost = [1096594666] * 43
+sum_ost = [1000000000] * 43
 b = [0] * 43
 s_max = 0
 l = 0
@@ -16,7 +16,7 @@ for i in range(n):
     if s - sum_ost[ost] > s_max:
         s_max = s - sum_ost[ost]
         l = i - b[ost]
-    if sum_ost[ost] == 1096594666:
+    if sum_ost[ost] == 1000000000:
         sum_ost[ost] = s
         b[ost] = i
 
