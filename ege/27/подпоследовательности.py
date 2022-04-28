6dd6ace58c7b0d69253722c10760d20e79daa0b0
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-04-29 01:40:57 +0300

    Create подпоследовательности.py

---
+++ ege/27/подпоследовательности.py
@@ -0,0 +1,13 @@
+'''Необходимо определить максимальное произведение подпоследовательности, состоящей из одного или более идущих подряд элементов.'''
+
+f = open('27A.txt')
+n = int(f.readline())
+d = [int(x) for x in f]
+max_pr = -10000000000000000000000000000
+for i in range(n):
+    cur_pr = d[i]
+    max_pr = max(max_pr, cur_pr)
+    for j in range(i+1, n):
+        cur_pr *= d[j]
+        max_pr = max(max_pr, cur_pr)
+print(max_pr)
