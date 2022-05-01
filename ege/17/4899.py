57eee8e547d5dafcac7b77d422e367d151f27c52
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-05-01 13:13:10 +0300

    Create 4899.py

---
+++ ege/17/4899.py
@@ -0,0 +1,27 @@
+f = open('17-272.txt')
+d = []
+while True:
+    s = f.readline()
+    if s == '':
+        break
+    d.append(int(s))
+cnt, sm = 0, 0
+for i in range(len(d)):
+    if d[i] > 0:
+        cnt += 1 
+        sm += d[i]
+ave = sm / cnt
+cnt2, max_s = 0, 0
+for i in range(len(d)-2):
+    if (d[i] > ave) or (d[i+1] > ave):
+        cnt2 += 1
+        s1 = str(abs(d[i]))
+        s2 = str(abs(d[i+1]))
+        sm1, sm2 = 0, 0
+        for i in range(len(s1)):
+            sm1 += int(s1[i])
+        for i in range(len(s2)):
+            sm2 += int(s2[i])
+        max_s = max(sm1, sm2)
+
+print(cnt2, max_s)
