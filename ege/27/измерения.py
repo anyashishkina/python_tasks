8cf77ca86ec925658f0be39154c86de379a4fd03
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-05-11 23:19:34 +0300

    Update измерения.py

--- ege/27/измерения.py
+++ ege/27/измерения.py
@@ -9,3 +9,20 @@ for i in range(n-5):
     for j in range(i+5, n):
         min_sum = min(min_sum, d[i]**2 + d[j]**2)
 print(min_sum)
+
+
+
+f = open('27B.txt')
+n = int(f.readline())
+d = [int(x) for x in f]
+min1, min2, ind1, ind2 = 1000000, 1000000, 0, 0
+for i in range(n):
+    if d[i] < min1:
+        min1 = d[i]
+        min2 = min1 
+        ind1 = i
+        ind2 = ind1
+    elif d[i] < min2:
+        min2 = d[i] 
+        ind2 = i
+print(min1**2 + min2**2)
