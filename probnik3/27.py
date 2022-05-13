3d3b15fbd60493977f868defada0332b71d5a35d
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-05-13 20:07:17 +0300

    Create 27.py

---
+++ probnik3/27.py
@@ -0,0 +1,10 @@
+f =  open('27-76a.txt')
+n = int(f.readline())
+d = [int(x) for x in f]
+cnt = 0
+for i in range(n):
+    for j in range(i+1, n):
+        if (len(d[i:j]) >= 1):
+            if ((d[i] + d[j])%3 == 0) and (sum(d[i:j])%2 == 0):
+                cnt += 1
+print(cnt)
