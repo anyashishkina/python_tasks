203611ee3307b3f90f4dee6edf36a658438380c0
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-05-13 20:32:18 +0300

    Update 27.py

--- probnik3/27.py
+++ probnik3/27.py
@@ -3,7 +3,7 @@ n = int(f.readline())
 d = [int(x) for x in f]
 cnt = 0
 for i in range(n):
-    for j in range(i+1, n):
+    for j in range(i+2, n):
         if (len(d[i:j]) >= 1):
             if ((d[i] + d[j])%3 == 0) and (sum(d[i:j])%2 == 0):
                 cnt += 1
