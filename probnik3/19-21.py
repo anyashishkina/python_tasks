4dea1d0fd7a6ac329b83544fb4f56e80bc6bb714
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-05-14 18:20:35 +0300

    Update 19-21.py

--- probnik3/19-21.py
+++ probnik3/19-21.py
@@ -1,15 +1,13 @@
 def game(k, h):
-    if (h == 3) and (k >= 65) and (k <= 100):
+    if (h == 2) and (k >= 65) and (k <= 100):
         return True
-    if (h == 3) and ((k < 65) or (k > 100)):
+    if (h == 2) and ((k < 65) or (k > 100)):
         return False
-    if (h < 3) and (k >= 65) and (k <= 100):
+    if (h == 1) and (k >= 65) and (k <= 100):
         return False
     h += 1
-    if (h == 1) or (h == 3):
-        return game(k +1, h) or game(k * 3, h)
-    if (h == 2):
-        return game(k +1, h) and game(k * 3, h)
+    return game(k +1, h) or game(k * 3, h)
+    
 for x in range(1, 64+1):
     if game(x, 0):
         print(x)
