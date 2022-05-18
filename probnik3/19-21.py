4f840a9dfcb5c1a36682d05865e78488e92a17f9
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-05-18 11:02:17 +0300

    Update 19-21.py

--- probnik3/19-21.py
+++ probnik3/19-21.py
@@ -5,8 +5,62 @@ def game(k, h):
         return False
     if (h == 1) and (k >= 65) and (k <= 100):
         return False
+    if (h == 1) and (k > 100):
+        return True
     h += 1
-    return game(k +1, h) or game(k * 3, h)
+   return game(k +1, h) or game(k * 3, h)
+    
+for x in range(1, 64+1):
+    if game(x, 0):
+        print(x)
+        break
+        
+        
+        
+def game(k, h):
+    if (h == 3) and (k >= 65) and (k <= 100):
+        return True
+    if (h == 3) and ((k < 65) or (k > 100)):
+        return False
+    if (h == 1) and (k > 100):
+        return False
+    if (h < 3) and (k >= 65) and (k <= 100):
+        return False
+    if (h == 2) and (k > 100):
+        return True
+    h += 1
+    if (h == 1) or (h == 3):
+        return game(k +1, h) or game(k * 3, h)
+    if (h == 2):
+         return game(k +1, h) and game(k * 3, h)
+        
+    
+for x in range(1, 64+1):
+    if game(x, 0):
+        print(x)
+        
+        
+        
+        
+        
+        
+ def game(k, h):
+    if ((h == 2) or (h == 4)) and (k >= 65) and (k <= 100):
+        return True
+    if (h == 4) and ((k < 65) or (k > 100)):
+        return False
+    if (h == 2) and (k > 100):
+        return False
+    if (h < 4) and (k >= 65) and (k <= 100):
+        return False
+    if ((h == 1) or (h == 3)) and (k > 100):
+        return True
+    h += 1
+    if (h == 1) or (h == 3):
+        return game(k +1, h) and game(k * 3, h)
+    if (h == 2) or (h == 4):
+         return game(k +1, h) or game(k * 3, h)
+        
     
 for x in range(1, 64+1):
     if game(x, 0):
