a83fb05e90c61f77818a7a3cd379a309cf4b355d
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-05-20 23:29:29 +0300

    Update 35485.py

--- ege/27/35485.py
+++ ege/27/35485.py
@@ -1,3 +1,17 @@
+f = open('27-A (5).txt')
+n = int(f.readline())
+a = [int(x) for x in f]
+ans = []
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            if (a[i] + a[j] +a[k]) % 3 == 0:
+                ans.append(a[i] + a[j] +a[k])
+print(max(ans))
+            
+
+    
+    
 f = open('27-B (3).txt')
 n = int(f.readline())
 m = [ [] for i in range(3)]
@@ -12,8 +26,8 @@ for i in range(3):
 
 ans = []
 for i in range(len(a)):
-    for j in range(len(a)):
-        for k in range(len(a)):
+    for j in range(i+1, len(a)):
+        for k in range(j+1, len(a)):
             if (a[i] + a[j] + a[k]) % 3 == 0:
                 ans.append(a[i] + a[j] + a[k])
 print(max(ans))
