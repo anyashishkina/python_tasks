fa6a48310afdc813c8a0a8ef3813e8a07a1a19b0
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-05-20 20:32:46 +0300

    Update 19-21.py

--- probnik4/19-21.py
+++ probnik4/19-21.py
@@ -12,3 +12,42 @@ for x in range(100):
     if game(7, x, 0):
         print(x)
         break
+
+        
+        
+def game(l, r, h):
+    if (h == 3) and ((l+r)>=45):
+        return True
+    if (h == 3) and ((l+r)<45):
+        return False
+    if (h < 3) and ((l+r)>=45):
+        return False
+    h += 1
+    if (h == 1) or (h == 3):
+        return game(l + r, r, h) or game(l, r+l, h)
+    if h == 2:
+        return game(l + r, r, h) and game(l, r+l, h)
+
+for x in range(100):
+    if game(6, x, 0):
+        print(x)
+        
+        
+def game(l, r, h):
+    if ((h == 2) or (h == 4)) and ((l+r)>=45):
+        return True
+    if (h == 4) and ((l+r)<45):
+        return False
+    if (h < 4) and ((l+r)>=45):
+        return False
+    h += 1
+    if (h == 2) or (h == 4):
+        return game(l + r, r, h) or game(l, r+l, h)
+    if (h == 1) or (h == 3):
+        return game(l + r, r, h) and game(l, r+l, h)
+
+for x in range(100):
+    if game(x, x, 0):
+        print(x)
+        break
+        
