cb076ad6e53fa04d67e9cf9d4c9b2e9df58ec624
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-06-04 15:38:03 +0300

    Update 24.py

--- probnik5/24.py
+++ probnik5/24.py
@@ -1,12 +1,13 @@
 f = open('24-171.txt').read()
 strings = f.split('\n')
 
-cur_len, max_len = 0, 0
+cur_len, max_len = 1, 0
 
 for s in strings:
-    for i in range(len(s)-2):
-        if s[i] == 'X' and s[i+1] == 'Y' and s[i+2] == 'Z':
-            cur_len += 3
+    s = s.replace('XYZ', '*')
+    for i in range(len(s)):
+        if s[i] == '*':
+            cur_len += 1
             max_len = max(max_len, cur_len)
         else:
             cur_len = 1
