2ed166fdbdeeb6dee22022b245516cd15b6ed5b9
anyashishkina <95246399+anyashishkina@users.noreply.github.com>
2022-06-04 14:09:41 +0300

    Create 19-21.py

---
+++ probnik5/19-21.py
@@ -0,0 +1,19 @@
+def game(k, h):
+    if (h == 2) and (k >= 36) and (k <= 60):
+        return True
+    if (h == 2) and ((k < 36) or (k > 60)):
+        return False
+    if (h == 1) and (k >= 36) and (k <= 60):
+        return False
+    if (h == 1) and (k > 60):
+        return True
+    h += 1
+    if h == 1:
+        return game(k + 1, h) and game(k * 2, h) and game(k * 3, h)
+    if h == 2:
+        return game(k + 1, h) or game(k * 2, h) or game(k * 3, h)
+
+for x in range(1, 35+1):
+    if game(x, 0):
+        print(x)
+        break
